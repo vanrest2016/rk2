3c787277317b453d986e71c9ebdf24cf57457d5c
vanrest2016 <56249174+vanrest2016@users.noreply.github.com>
2019-11-11 11:26:00 +0300

    Update index.php

--- index.php
+++ index.php
@@ -14,11 +14,7 @@ for($j = 0; $j <= 10; $j++)
 $num=$j*$i;
 echo '<td>';
 if($i==0 || $j==0) echo $i,$j;
-if(($num)%2==0);
-elseif(($num)%3==0);
-elseif(($num)%5==0);
-else echo $num;
-}
+else echo $num;}
 }
 ?>
 </table>
